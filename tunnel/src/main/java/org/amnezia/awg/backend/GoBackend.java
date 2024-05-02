b0580324e25b286aa40e9e639fe5689b901f1bed
Zane Schepke <zanecschepke@gmail.com>
2024-05-02 11:41:49 -0400

    feat: allow parallel import with wg

--- tunnel/src/main/java/org/amnezia/awg/backend/GoBackend.java
+++ tunnel/src/main/java/org/amnezia/awg/backend/GoBackend.java
@@ -58,7 +58,7 @@ public final class GoBackend implements Backend {
      * @param context An Android {@link Context}
      */
     public GoBackend(final Context context) {
-        SharedLibraryLoader.loadSharedLibrary(context, "wg-go");
+        SharedLibraryLoader.loadSharedLibrary(context, "am-go");
         this.context = context;
     }
 
