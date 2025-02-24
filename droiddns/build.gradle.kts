2db31a52f7d1c85c40b0594cb0c2bfa673360030
Zane Schepke <zanecschepke@gmail.com>
2025-02-23 21:57:19 -0500

    bump versions

--- droiddns/build.gradle.kts
+++ droiddns/build.gradle.kts
@@ -56,7 +56,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.zaneschepke"
             artifactId = "droiddns"
-            version = "1.0.1"
+            version = "1.0.2"
             afterEvaluate {
                 from(components["release"])
             }
