21d97591ab162c7483e6205ea4abe03e79d0e9b1
Zane Schepke <dev@zaneschepke.com>
2025-04-06 18:34:26 -0400

    fix: ipv6 static regex bug

--- droiddns/src/main/java/com/zaneschepke/droiddns/JavaDnsResolver.kt
+++ droiddns/src/main/java/com/zaneschepke/droiddns/JavaDnsResolver.kt
@@ -24,20 +24,15 @@ class JavaDnsResolver @JvmOverloads constructor(
         private val TAG = JavaDnsResolver::class.java.simpleName
 
         private val IPV6_REGEX = Regex(
-            "^(?:" +
-                    "\\[(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}]|" +
-                    "\\[::(?:[0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}]|" +
-                    "\\[[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,5}[0-9a-fA-F]{1,4}]|" +
-                    "\\[(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}]|" +
-                    "\\[(?:[0-9a-fA-F]{1,4}:){1,7}:]|" +
-                    "\\[::]|" +
-                    "(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}|" +
-                    "::(?:[0-9a-fA-F]{1,4}:){0,6}[0-9a-fA-F]{1,4}|" +
-                    "[0-9a-fA-F]{1,4}::(?:[0-9a-fA-F]{1,4}:){0,5}[0-9a-fA-F]{1,4}|" +
-                    "(?:[0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|" +
-                    "(?:[0-9a-fA-F]{1,4}:){1,7}:|" +
-                    "::" +
-                    ")$"
+            "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:)" +
+                    "{1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]"+
+                    "{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:"+
+                    "[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4})"+
+                    "{1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}"+
+                    ":((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]"+
+                    "{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}"+
+                    "[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:)"+
+                    "{1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))"
         )
         private val IPV4_REGEX = Regex(
             "^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}" +
@@ -61,8 +56,10 @@ class JavaDnsResolver @JvmOverloads constructor(
     @Synchronized
     override fun resolveDns(hostname: String, preferIpv4: Boolean, useCache: Boolean): List<String> {
         Log.i(TAG, "Starting DNS resolution for hostname=$hostname, preferIpv4=$preferIpv4, useCache=$useCache")
-        if (IPV4_REGEX.matches(hostname) || IPV6_REGEX.matches(hostname)) {
-            Log.d(TAG, "Hostname $hostname is already an IP address, returning as-is")
+
+
+        if (IPV4_REGEX.matches(hostname) || IPV6_REGEX.matches(hostname.removeSurrounding("[", "]"))) {
+            Log.i(TAG, "Hostname already resolved: $hostname. Returning as is.")
             return listOf(hostname)
         }
 
